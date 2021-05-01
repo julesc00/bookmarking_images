631fd05972a165a2cacb173b3eaa63cf47cf41a4
raulctrlsun <raul@ctrlsun.com>
2021-05-01 18:29:02 -0500

    Added logic for getting third-party image url.

--- images/forms.py
+++ images/forms.py
@@ -1,4 +1,8 @@
+from urllib import request
 from django import forms
+from django.core.files.base import ContentFile
+from django.utils.text import slugify
+
 
 from .models import Image
 
@@ -10,3 +14,31 @@ class ImageCreateForm(forms.ModelForm):
         widgets = {
             "url": forms.HiddenInput
         }
+
+    def clean_url(self):
+        url = self.cleaned_data["url"]
+        valid_extensions = ["jpg", "jpeg", "png"]
+        extension = url.rsplit(".", 1)[1].lower()
+
+        if extension not in valid_extensions:
+            raise forms.ValidationError("The given URL does not match valid image extensions.")
+
+        return url
+
+    def save(self, force_insert=False, force_update=False, commit=True):
+        """Override to retrieve the given image and save it."""
+
+        image = super().save(commit=False)
+        image_url = self.cleaned_data["url"]
+        name = slugify(image.title)
+        extension = image_url.rsplit(".", 1)[1].lower()
+        image_name = f"{name}.{extension}"
+
+        # Download image from the given URL
+        response = request.urlopen(image_url)
+        image.image.save(image_name, ContentFile(response.read()), save=False)
+
+        if commit:
+            image.save()
+
+        return image
